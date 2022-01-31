5d1e128aee9ae607cb67989e5ee9ca6915656613
Finleif <58663789+Finleif@users.noreply.github.com>
2022-01-31 21:54:15 +0100

    started on listView in database

---
+++ app/src/main/java/no/hvl/dat153/DatabaseActivity.java
@@ -0,0 +1,19 @@
+package no.hvl.dat153;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
+}
+
+// lage en fake database "arraylist"
+
+// skal kunne skrolles i. Add entry, delete entry, sortere alfabetisk og reverse
+
--- app/src/main/java/no/hvl/dat153/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/MainActivity.java
@@ -2,13 +2,30 @@ package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button dbButton = findViewById(R.id.databasebutton);
+
+        /** sends user from the database button to the actual database activity*/
+        dbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DatabaseActivity.class));
+            }
+        });
     }
 }
+
+
+// mål er å sende videre til database aktivitet fikset
